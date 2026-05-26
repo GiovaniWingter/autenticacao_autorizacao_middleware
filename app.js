15aa29064a68bd381799b5ba51cd7cf13b9b200d
Giovani Wingter <giovaniwingter@hotmail.com>
2026-05-26 16:34:10 -0300

    Ajustes

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require("express");
+//Importa o módulo express para criar o servidor web. 
+
 const app = express();
+//Cria o servidor express e o armazena na constante app
+
+var session = require("express-session");
+//importa o módulo express-session para gerenciar sessões de usuário.
+
+app.use(
+  session({
+    secret: "HELLonODE",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+  })
+);
+// Configura o middleware de sessão para o aplicativo express. 
+// O segredo é usado para assinar a ID da sessão, 
+// resave e saveUninitialized controlam o comportamento de salvamento da sessão,
+// e cookie define as opções do cookie de sessão.
 
 const env = require("dotenv").config();
-var session = require("express-session");
-
 app.use(express.static("./app/public"));
 
 app.set("view engine", "ejs");
@@ -11,19 +28,9 @@ app.set("views", "./app/views");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(
-  session({
-    secret: "HELLonODE",
-    resave: false,
-    saveUninitialized: false,
-  }));
-
 var rotas = require("./app/routes/router");
 app.use("/", rotas);
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`Servidor online...\nhttp://localhost:${process.env.APP_PORT}`);
 }); 
-
-// teste
--- app/middlewares/autenticadorMiddleware.js
+++ app/middlewares/autenticadorMiddleware.js
@@ -2,12 +2,37 @@ const { validationResult } = require("express-validator");
 const usuario = require("../models/usuarioModel");
 const bcrypt = require("bcryptjs");
 
-verificarUsuAutenticado = (req, res, next) => {
+gravarUsuAutenticado = async (req, res, next) => {
     let autenticado = { usuLogado: null, id: null, tipo: null };
-    if (req.session.autenticado) {
-        autenticado = req.session.autenticado;
-    } 
+    const erros = validationResult(req);
+    if (erros.isEmpty()) {
+        try {
+            var dadosForm = {
+                user_usuario: req.body.nome_usu,
+                senha_usuario: req.body.senha_usu,
+            };
+            let results = await usuario.findUserEmail(dadosForm);
+            if (results.length === 1) {
+                if (await bcrypt.compare(dadosForm.senha_usuario, results[0].senha_usuario)) {
+                    autenticado = {
+                        usuLogado: results[0].nome_usuario,
+                        id: results[0].id_usuario,
+                        tipo: results[0].tipo_usuario
+                    };
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
     req.session.autenticado = autenticado;
+    next();
+}
+
+verificarUsuAutenticado = (req, res, next) => {
+    if (!req.session.autenticado) {
+        req.session.autenticado = { usuLogado: null, id: null, tipo: null };
+    }
     next();
 }
 
@@ -16,44 +41,17 @@ limparSessao = (req, res, next) => {
     next()
 }
 
-gravarUsuAutenticado = async (req, res, next) => {
-    let autenticado =  { usuLogado: null, id: null, tipo: null };
-    erros = validationResult(req)
-    if (erros.isEmpty()) {
-        var dadosForm = {
-            user_usuario: req.body.nome_usu,
-            senha_usuario: req.body.senha_usu,
-        };
-        let results = await usuario.findUserEmail(dadosForm);
-        let total = Object.keys(results).length;
-        if (total == 1) {
-            if (bcrypt.compareSync(dadosForm.senha_usuario, results[0].senha_usuario)) {
-                autenticado = {
-                    usuLogado: results[0].nome_usuario,
-                    id: results[0].id_usuario,
-                    tipo: results[0].tipo_usuario
-                };
-            }
-        } 
-    } 
-    req.session.autenticado = autenticado;
-    next();
-}
-
 verificarUsuAutorizado = (tipoPermitido, destinoFalha) => {
     return (req, res, next) => {
         if (req.session.autenticado.usuLogado != null &&
-            tipoPermitido.find( (element)=> { return element == req.session.autenticado.tipo }) != undefined) {
+            tipoPermitido.includes(req.session.autenticado.tipo)) {
             next();
         } else {
-            res.render(destinoFalha, req.session.autenticado);
+            res.render(destinoFalha, { autenticado: req.session.autenticado });
         }
     };
 }
 
 module.exports = {
-    verificarUsuAutenticado,
-    limparSessao,
-    gravarUsuAutenticado,
-    verificarUsuAutorizado
+    verificarUsuAutenticado, limparSessao, gravarUsuAutenticado, verificarUsuAutorizado
 }
